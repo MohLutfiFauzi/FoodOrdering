a37d37eb3f925ec12d3bee32c4f5bba8a4a9015f
moh.fauzi <moh.fauzi@ist.id>
2024-05-09 15:28:06 +0700

    first custom component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import Colors from "@/src/constants/Colors";
+
+type ProductData = {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+};
+
+export const ProductListItem = ({ product }: { product: ProductData }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "700",
+  },
+});
